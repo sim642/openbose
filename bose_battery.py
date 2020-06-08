04abe3aa7d656dbe52aa9406750182e4e9d5239a
Simmo Saan <simmo.saan@gmail.com>
2020-06-08 11:38:24 +0300

    Add NOW_PLAYING read to bose_battery.py

--- bose_battery.py
+++ bose_battery.py
@@ -5,32 +5,38 @@ from bose import *
 address = "4C:87:5D:53:F2:CF"
 channel = 8
 
-print("Connecting...")
 with Connection(address, channel) as conn:
-    print("Connected")
 
-    def command(packet: Packet):
-        print("Sending...:", packet)
+    def write(packet: Packet):
+        print("-->", packet)
         conn.write(packet)
-        print("Sent")
 
-        print("Receiving...")
+    def read() -> Packet:
         packet = conn.read()
-        print("Received:", packet)
+        print("<--", packet)
+        return packet
 
 
-    command(Packet(FunctionBlock.PRODUCT_INFO, ProductInfoFunction.BMAP_VERSION, Operator.GET))
-    command(Packet(FunctionBlock.PRODUCT_INFO, ProductInfoFunction.PRODUCT_ID_VARIANT, Operator.GET))
-    command(Packet(FunctionBlock.PRODUCT_INFO, ProductInfoFunction.SERIAL_NUMBER, Operator.GET))
-    command(Packet(FunctionBlock.PRODUCT_INFO, ProductInfoFunction.FIRMWARE_VERSION, Operator.GET))
+    def command_get(packet: Packet):
+        write(packet)
+        read()
 
-    command(Packet(FunctionBlock.STATUS, StatusFunction.BATTERY_LEVEL, Operator.GET))
+    def command_start(packet: Packet):
+        write(packet)
+        processing_packet = Packet(packet.function_block, packet.function, Operator.PROCESSING)
+        result_packet = Packet(packet.function_block, packet.function, Operator.RESULT)
+        assert read() == processing_packet
+        while read() != result_packet:
+            pass
 
-    command(Packet(FunctionBlock.AUDIO_MANAGEMENT, AudioManagementFunction.STATUS, Operator.GET))
-    command(Packet(FunctionBlock.AUDIO_MANAGEMENT, AudioManagementFunction.VOLUME, Operator.GET))
-    # command(bytes([5, 6, 5, 0])) # audio now playing
-    # print(s.recv(128))
-    # print(s.recv(128))
-    # print(s.recv(128))
-    # print(s.recv(128))
-    # print(s.recv(128))
+
+    command_get(Packet(FunctionBlock.PRODUCT_INFO, ProductInfoFunction.BMAP_VERSION, Operator.GET))
+    # command_get(Packet(FunctionBlock.PRODUCT_INFO, ProductInfoFunction.PRODUCT_ID_VARIANT, Operator.GET))
+    # command_get(Packet(FunctionBlock.PRODUCT_INFO, ProductInfoFunction.SERIAL_NUMBER, Operator.GET))
+    # command_get(Packet(FunctionBlock.PRODUCT_INFO, ProductInfoFunction.FIRMWARE_VERSION, Operator.GET))
+
+    command_get(Packet(FunctionBlock.STATUS, StatusFunction.BATTERY_LEVEL, Operator.GET))
+
+    # command_get(Packet(FunctionBlock.AUDIO_MANAGEMENT, AudioManagementFunction.STATUS, Operator.GET))
+    # command_get(Packet(FunctionBlock.AUDIO_MANAGEMENT, AudioManagementFunction.VOLUME, Operator.GET))
+    command_start(Packet(FunctionBlock.AUDIO_MANAGEMENT, AudioManagementFunction.NOW_PLAYING, Operator.START))
